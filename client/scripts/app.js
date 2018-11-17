c45a70a228d16ca61c02fb1a7794532e30b493ae
klarok <karaylu@gmail.com>
2018-11-16 17:10:30 -0800

    Refactor messagesView to render all messages
      - accounted for nonexisting usernames and texts
      - added anchor tags to usernames in MessageView template

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,8 +20,9 @@ var App = {
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
-      console.log(data);
-
+      // console.log(data);
+      // MessagesView.renderMessage(data.results);
+      MessagesView.render(data.results);
       callback();
     });
   },
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,12 +5,21 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
+  render: function(results) {
+    for (var i = 0; i < results.length; i++) {
+      this.renderMessage(results[i]);
+    }
   },
   
   renderMessage: function(message) {
+    if (message.username === undefined || message.text === undefined) {
+      console.log('username or text does not exist');
+      return;
+    }
+    
     let chat = MessageView.render(message);
-    this.$chats.append(chat);
+    this.$chats.before(chat);
+    //return chat;
   }
 
 };
